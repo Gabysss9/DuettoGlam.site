a9be9d966a304ff35f7d3041ea891c667853a619
Yasmin Abreu <gabryelleyasmin3@gmai.com>
2025-03-14 17:53:20 -0300

    js

--- login.js
+++ login.js
@@ -1,62 +1,17 @@
-let emailInput = document.getElementById('email'); 
-let emailLabel = document.querySelector('label[for="email"]'); 
-let emailHelper = document.querySelector('.helper'); 
+let inputEmail = document.getElementById("email");
+let inputPassword = document.getElementById("password");
+let formulário = document.querySelector("form");
 
-function togglePopup(input, label) { 
-  // Defina a lógica da função
-}  
+inputEmail.addEventListener("focus", ()=>{
+  inputEmail.style.backgroundColor = "lightgreen";
 
-function inputCorreto(input, label, helper) {
-  input.classList.remove("error"); 
-  input.classList.add("correct"); 
-  helper.classList.remove("visible");
-} 
-
-function inputIncorreto(input, label, helper) {
-  input.classList.add("error"); 
-  input.classList.remove("correct"); 
-  helper.classList.add("visible");
-} 
-
-emailInput.addEventListener('change', (e) => { 
-  let valor = e.target.value; 
-  if (valor.includes('@') && valor.includes('.com')) { 
-    inputCorreto(emailInput, emailLabel, emailHelper); 
-  } else { 
-    emailHelper.innerText = "Precisa inserir um email válido"; 
-    inputIncorreto(emailInput, emailLabel, emailHelper); 
-  } 
-}); 
-
-let passwordInput = document.getElementById('password'); 
-let passwordLabel = document.querySelector('label[for="senha"]'); 
-let passwordHelper = document.querySelector('.helper'); 
-
-passwordInput.addEventListener('blur', (e) => { 
-  let valor = e.target.value; 
-  if (valor.length > 3) { 
-    inputCorreto(passwordInput, passwordLabel, passwordHelper); 
-  } else { 
-    passwordHelper.innerText = "Precisa inserir uma senha válida"; 
-    inputIncorreto(passwordInput, passwordLabel, passwordHelper); 
-  } 
-});  
-
-console.log(emailInput, emailLabel, emailHelper); 
-console.log(passwordInput, passwordLabel, passwordHelper);  
-
-let botao = document.querySelector("button"); 
-botao.addEventListener('click', (e) => { 
-  e.preventDefault(); 
-  alert("Login efetuado com sucesso"); 
 });
 
+inputEmail.addEventListener("blur", ()=>{
+  e.target.style.backgroundColor = "";
+});
 
-let button = document.querySelector("button");
-let span = document.querySelector("span");
+formulario.addEventListener("submit", ()=> {
+  alert("Dados enviados com sucesso!")
 
-function mostrarSpan(){
-
-span.style.opacity= "100"
-}
-button.addEventListener("mouseover", mostrarSpan);
+});
